1f8f59dad01075310b43c1bf71b447f04cef7676
Parth Jethwa <parth.jethwa@sparsh1.com>
2017-10-17 15:22:47 +0530

    temporary loader for services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
-
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './services/in-memory-data.service';
+import { LoadingModule, ANIMATION_TYPES } from 'ngx-loading';
 
 //components
 import { AppComponent } from './app.component';
@@ -18,13 +18,13 @@ import { SearchComponent } from './components/search/search.component';
 import { TrendsComponent } from './components/trends/trends.component';
 import { ProductsListComponent } from './components/products/products-list/products-list.component';
 import { ProductsDetailsComponent } from './components/products/products-details/products-details.component';
+
 //route of the application
 import { AppRoutingModule } from './app-routing.module';
 
 //services
 import { ProductsService } from './services/products.service';
 import { ConstantsService } from './services/constants.service';
-
 
 @NgModule({
 	declarations: [
@@ -45,6 +45,14 @@ import { ConstantsService } from './services/constants.service';
 		AppRoutingModule,
 		InMemoryWebApiModule.forRoot(InMemoryDataService),
 		HttpModule,
+		LoadingModule.forRoot({
+			animationType: ANIMATION_TYPES.threeBounce,
+			backdropBackgroundColour: 'rgba(0,0,0,0.1)',
+			backdropBorderRadius: '4px',
+			primaryColour: '#ffffff',
+			secondaryColour: '#ffffff',
+			tertiaryColour: '#ffffff'
+		})
 	],
 	providers: [ProductsService, ConstantsService],
 	bootstrap: [AppComponent]
